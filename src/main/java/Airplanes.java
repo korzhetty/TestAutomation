bf0fb85658e8d187d7d338df9b887390e1ec3044
Anna Korzh <anna@a.korzh-ubuntu>
2019-03-06 16:01:51 +0200

    Initial commit

---
+++ src/main/java/Airplanes.java
@@ -0,0 +1,22 @@
+public class Airplanes
+
+{
+    int airplanesCapacity;
+    int maxAirplanesSpeed;
+    int airplainesWingspan;
+    int numberOfEngines;
+    int airplainsWeight;
+
+}
+
+    void downTheWheels() {
+    }
+
+    void runEngines(){
+    }
+
+    void takeWing(){
+    }
+
+}
+
